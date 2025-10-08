7875189a19cfcb43a79fa51c6306e50f53a920c0
r3gb0b2 <rafael@agenciavitrine.com>
2025-10-08 16:08:13 -0300

    feat: Refactor promoter data structure and admin panel
    Updates the `Promoter` and `PromoterApplicationData` types to include fields like `status`, `createdAt`, and `rejectionReason`. The admin panel is refactored to leverage these updated types and integrate better with Firebase authentication and services. This includes adding a logout functionality and improving the management of promoter data within the panel.

--- types.ts
+++ types.ts
@@ -1,30 +1,32 @@
-import { Timestamp } from 'firebase/firestore';
+import { FieldValue } from 'firebase/firestore';
+
+export type PromoterStatus = 'pending' | 'approved' | 'rejected';
+
+export interface Promoter {
+  id: string;
+  name: string;
+  whatsapp: string;
+  email: string;
+  instagram: string;
+  tiktok?: string;
+  dateOfBirth: string;
+  photoUrls: string[];
+  status: PromoterStatus;
+  createdAt: FieldValue;
+  rejectionReason?: string;
+}
 
 export interface PromoterApplicationData {
   name: string;
   whatsapp: string;
   email: string;
   instagram: string;
-  tiktok: string;
+  tiktok?: string;
   dateOfBirth: string;
   photos: File[];
 }
 
-export interface Promoter {
-  id: string;
-  name: string;
-  whatsapp: string;
-  email:string;
-  instagram: string;
-  tiktok: string;
-  dateOfBirth: string;
-  photoUrls: string[];
-  status: 'pending' | 'approved' | 'rejected';
-  createdAt: Timestamp;
-  rejectionReason?: string;
-}
-
 export interface RejectionReason {
-    id: string;
-    text: string;
+  id: string;
+  text: string;
 }
